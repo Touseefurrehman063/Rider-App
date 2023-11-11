6c72fd3f201bb03d8073fe30ee9b3afb39aa7134
darklips97351 <dark.lips1@outlook.com>
2023-11-11 13:05:02 +0500

    cmt

--- lib/Screen/Welcome_Screens/_welcome_3.dart
+++ lib/Screen/Welcome_Screens/_welcome_3.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riderapp/Screen/Login/_login.dart';
 import 'package:flutter_riderapp/Screen/Welcome_Screens/_welcome_4.dart';
 import 'package:flutter_riderapp/main.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 class Welcome3 extends StatefulWidget {
   const Welcome3({super.key});
@@ -25,7 +24,7 @@ class _Welcome3State extends State<Welcome3> {
             alignment: Alignment.topRight,
             child: Padding(
               padding:
-                   EdgeInsets.symmetric(horizontal: 10.0, vertical: Get.height*0.045),
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
               child: TextButton(
                 onPressed: () {
                   print("button pressed");
@@ -45,7 +44,7 @@ class _Welcome3State extends State<Welcome3> {
             ),
           ),
           Column(
-            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.09,
@@ -61,27 +60,20 @@ class _Welcome3State extends State<Welcome3> {
                 ),
               ),
               SizedBox(
-                height: MediaQuery.of(context).size.height * 0.1,
+                height: MediaQuery.of(context).size.height * 0.09,
               ),
-              Padding(
-                padding:  EdgeInsets.only(bottom:Get.height*0.013),
-                child: Image.asset('assets/Frame1.png',height: Get.height*0.38,),
-              ),
-              SizedBox(height: Get.height*0.06,),
-              Padding(
-                padding:  EdgeInsets.only(bottom:Get.height*0.06),
-                child: SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.1,
-                  child: InkWell(
-                    onTap: () {
-                      log(initScreen.toString());
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const Welcome4()),
-                      );
-                    },
-                    child: Image.asset('assets/arrow.png'),
-                  ),
+              Image.asset('assets/Frame1.png'),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.09,
+                child: InkWell(
+                  onTap: () {
+                    log(initScreen.toString());
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Welcome4()),
+                    );
+                  },
+                  child: Image.asset('assets/arrow.png'),
                 ),
               ),
             ],
